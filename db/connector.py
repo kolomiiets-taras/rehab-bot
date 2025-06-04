649e4959a4a64537ca8b1e1e35159a574f25bffb
Taras Kolomiiets <kolomiiets.t@ajax.systems>
2025-06-04 22:32:14 +0300

    fix add_first_admin

--- db/connector.py
+++ db/connector.py
@@ -1,3 +1,5 @@
+from sqlalchemy import select
+
 from .database import Base, engine, async_session
 from db.models import Employee, Role
 from passlib.hash import bcrypt
@@ -11,17 +13,21 @@ async def create_db_and_tables() -> None:
 
 async def add_first_admin() -> None:
     async with async_session() as session:
-        admin = Employee(
-            first_name="Admin",
-            last_name="Adminov",
-            email="owner@owner.com",
-            password=bcrypt.hash("1234"),
-            role=Role.OWNER
-        )
-        session.add(admin)
-        await session.commit()
-        if await session.get(Employee, admin.id):
-            print("Admin added successfully")
+        email = "owner@owner.com"
+        result = await session.execute(select(Employee).where(Employee.email == email))
+        existing_admin = result.scalar_one_or_none()
+        if not existing_admin:
+            admin = Employee(
+                first_name="Admin",
+                last_name="Adminov",
+                email=email,
+                password=bcrypt.hash("1234"),
+                role=Role.OWNER
+            )
+            session.add(admin)
+            await session.commit()
+            if await session.get(Employee, admin.id):
+                print("Admin added successfully")
 
 
 async def drop_db_tables() -> None:
